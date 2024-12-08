08c94e95c7aa6d940dad5dab40ab35b9b811f441
Gasolindo <brut.tall@hotmail.com>
2024-12-08 11:54:43 -0300

    PageLogin

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -1,11 +1,47 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-card width="400" class="mx-auto mt-10">
+      <v-card-title>
+        <h1 align="center" class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field width="330"
+            label="Matricula"
+            prepend-icon="mdi-account-circle"
+          />
+          <v-text-field
+          
+            v-model="password"
+            :type="showPassword ? 'text' : 'password'"
+            label="Senha"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="togglePasswordVisibility"
+          />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-row justify="center">
+          <v-btn color="success">Entrar</v-btn>
+        </v-row>
+      </v-card-actions>
+    </v-card>
   </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+  export default {
+    data() {
+      return {
+        password: '',
+        showPassword: false,
+      };
+    },
+    methods: {
+      togglePasswordVisibility() {
+        this.showPassword = !this.showPassword;
+      },
+    },
+  };
 </script>
